2650aa1ee419a66033076d8035653f9265e2d994
DFurtune <d.furtune18@gmail.com>
2024-07-13 13:43:45 +0300

    add changes

--- script.js
+++ script.js
@@ -1,55 +1,41 @@
 "use strict";
 
-let addBtn = document.querySelector("#addBtn");
-let titleTask = document.querySelector("#addTaskTitle");
-let toDoList = document.querySelector("#toDoList");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-function addTask(taskDescriptionText) {
-  if (taskDescriptionText.trim() !== "") {
-    let newTask = document.createElement("li");
-    newTask.classList.add("todo__item");
-
-    let label = document.createElement("label");
-    label.classList.add("label");
-    newTask.appendChild(label);
-
-    let input = document.createElement("input");
-    input.setAttribute("type", "checkbox");
-    label.appendChild(input);
-
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
     let span = document.createElement("span");
-    span.classList.add("checkmark");
-    label.appendChild(span);
-
-    let taskDescription = document.createElement("p");
-    taskDescription.classList.add("description");
-    taskDescription.innerText = taskDescriptionText;
-    label.appendChild(taskDescription);
-
-    let deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("delete_button");
-    newTask.appendChild(deleteBtn);
-
-    deleteBtn.addEventListener("click", () => {
-      toDoList.removeChild(newTask);
-    });
-
-    toDoList.appendChild(newTask);
-    titleTask.value = "";
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  inputBox.value = "";
+  saveData();
 }
 
-addBtn.addEventListener("click", () => {
-  if (titleTask.value.trim() !== "") {
-    addTask(titleTask.value);
-  }
-});
-
-titleTask.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    if (titleTask.value.trim() !== "") {
-      addTask(titleTask.value);
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
     }
-  }
-});
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
